cccfedb3ffa6a5e5d5aff5a5f2818bf28962ffdd
Satyam Gupta <satyamgupta11692@gmail.com>
2021-09-17 17:40:01 +0200

    First commit in react app

---
+++ src/components/NexExpense/NexExpense.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import  './NewExpense.css';
+
+
+function NewExpense(props){
+
+    const saveExpenseDataHandler= (eneteredExpenseData) =>{
+        const expenseData = {
+            ...eneteredExpenseData,
+            id:Math.random.toString()
+        }
+        props.onAddExpense(expenseData);
+    };
+    return <div className="new-expense">
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
+    </div>
+
+}
+
+export default NewExpense; 
