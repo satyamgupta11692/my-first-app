4da110f921a1ce15ddd9e6251fb8e1a6345c4430
Satyam Gupta <satyamgupta11692@gmail.com>
2021-09-19 17:37:03 +0200

    Removed console logs

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -7,9 +7,6 @@ const ChartBar = (props) =>{
     if(props.maxValue>0){
         barFillHeight=Math.round((props.value/props.maxValue)*100)+'%';
     }
-    console.log(props.value);
-    console.log(props.maxValue);
-    console.log(barFillHeight);
     return <div className="chart-bar">
         <div className="chart-bar__inner">
             <div className="chart-bar__fill" style={{height:barFillHeight}}></div>
